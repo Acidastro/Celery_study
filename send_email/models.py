92eeda3083ed829e0387f9dff12109d4886a6e6a
acidastro <acid1astro@gmail.com>
2023-01-31 20:08:02 +0400

    models

---
+++ send_email/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Contact(models.Model):
+    """Подписка по email"""
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+   
